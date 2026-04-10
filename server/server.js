adf515f870580cb13ecf7dc046e59ee4bbb2cccc
Kavyas-org <kavyathakur4433@gmail.com>
2026-04-10 15:28:38 +0530

    fix cors issue

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://your-vercel-domain.vercel.app",
+    "https://gammongroupcompanies.com"
   ],
   credentials: true
 }));
