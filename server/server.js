21ce71be0798d6d517d4b895bb3f4783c1d589fc
Kavyas-org <kavyathakur4433@gmail.com>
2026-04-10 14:23:02 +0530

    update

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 
 
 // middleware
@@ -40,7 +39,8 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-// server start
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`📦 Environment: ${process.env.NODE_ENV}`);
 });
